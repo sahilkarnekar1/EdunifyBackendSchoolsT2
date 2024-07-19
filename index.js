949584bfe2525bbb6d35f289e6ad414a4730d418
sahilkarnekar1 <sahilkarnekar1@gmail.com>
2024-07-19 17:16:33 +0530

    first commit

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/schoolImages', express.static(path.join(__dirname, 'schoolImages')));
 app.use('/api', schoolsRouter);
 
+app.get('/', (req, res) => res.send('API Running'));
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
